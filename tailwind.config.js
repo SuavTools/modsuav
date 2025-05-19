74a34336fce67394ed8d424be9754bbc24e1732f
SuavTools <ricardo.moreira.designs@gmail.com>
2025-05-19 18:05:37 +0000

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,10 @@ module.exports = {
   theme: {
     extend: {
   colors: {
-  suavred: '#e11d48',
-  backdrop: '#0f0f0f',
-  whisper: '#f1f1f1',
-  accent: '#63b8e7'
+    suav: '#63b8e7',         // Your signature blue
+    backdrop: '#0f0f0f',     // Console black
+    whisper: '#f1f1f1',      // Soft white
+    accent: '#e11d48'        // Optional red for emphasis if needed
  },
 fontFamily: {
   sans: ['Helvetica Neue', 'sans-serif']
