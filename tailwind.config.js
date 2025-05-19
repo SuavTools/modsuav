71cf528a05ce55237dd632620de5a4880b4c619b
SuavTools <ricardo.moreira.designs@gmail.com>
2025-05-19 17:47:28 +0000

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,15 @@ module.exports = {
   content: ["./**/*.njk", "./src/**/*.css", "./*.njk"],
   theme: {
     extend: {
-      colors: {
-        sujooo: '#e11d48',
-        backdrop: '#0f0f0f',
-        whisper: '#f1f1f1'
-      },
-      fontFamily: {
-        sans: ['Helvetica Neue', 'sans-serif'],
-      }
+  colors: {
+  suavred: '#e11d48',
+  backdrop: '#0f0f0f',
+  whisper: '#f1f1f1',
+  accent: '#63b8e7'
+ },
+fontFamily: {
+  sans: ['Helvetica Neue', 'sans-serif']
+}
     }
   },
   plugins: [],
