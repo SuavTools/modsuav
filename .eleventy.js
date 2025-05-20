3ac55ea18177741b14ce89d644a998de449828f0
SuavTools <ricardo.moreira.designs@gmail.com>
2025-05-20 01:52:31 +0000

    commit

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function (eleventyConfig) {
- eleventyConfig.addPassthroughCopy({ "public/logo.svg": "logo.svg" });
+  eleventyConfig.addPassthroughCopy({ "public/logo.svg": "logo.svg" });
+  eleventyConfig.addPassthroughCopy({ "public/intro.mp3": "intro.mp3" });
 
   return {
     dir: {
